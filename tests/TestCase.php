15503449872c5d410992693981a8b8d88175bea8
Diogo M. Oliveira <dmarostega@gmail.com>
2026-07-14 18:16:42 -0300

    test: align Inertia page path casing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,4 +4,12 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
-abstract class TestCase extends BaseTestCase {}
+abstract class TestCase extends BaseTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        config()->set('inertia.testing.page_paths', [resource_path('js/pages')]);
+    }
+}
